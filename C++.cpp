9d4cb9eb00ca8e75786b809acf66a687d36ff52b
nk <2191133162@qq.com>
2024-08-13 16:38:55 +0800

    Codeforces Beta Round 5 / C

---
+++ C++.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 21911 on 24-8-13.
+//
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a = {3, 2, 1};
+    ranges::sort(a);
+    cout << __cplusplus << endl; // 比如我的 C++ 版本是 199711，对比下面的参数是 C++98
+}
